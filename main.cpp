8d1a9727d09b2868140d8c077dff55527b95238d
Talon Baker <taalonn@gmail.com>
2014-12-07 13:42:02 -0800

    Pushing more changes
    Again, this will be tested because this it bound to cause lots of
    problems after all the times linked together.

--- main.cpp
+++ main.cpp
@@ -1,49 +1,49 @@
-#include"header.h"
+#include "Robot.h"
+#include "Earth.h"
+#include "Air.h"
+#include "Sea.h"
+#include "Accounts.h"
+#include "header.h"
 
+void MainMenu();
 int main()
 {
+
 	bool adminStatus = false;
 	Accounts test;
 	int selection;
 
-	//testing text/background color
-	system("color 12");
-
 	while (selection != 0 && selection != 2)
 	{
-		cout <<	  "\nPlease make a selection:\n"
-				  "1--Sign Up\n"
-				  "2--Log in\n"
-				  "0--Exit\n\n"
-				  "Selection: ";
-
-		cin  >> selection;
-		cin.ignore();
-
+		cout << "\nPlease make a selection:\n"
+		"1--Sign Up\n"
+		"2--Log in\n"
+		"0--Exit\n\n"
+		"Selection: ";
+		selection = GetValidInt(2,0);
 		switch(selection)
 		{
-		case 1: test.SignUp();
-				break;
-		case 2: cout << endl;
-				test.LogIn(adminStatus);
+			case 1: test.SignUp();
+			break;
+			case 2: cout << endl;
+			test.LogIn(adminStatus);
 
-				if (adminStatus == true)
-				{
-					test.AdminSelection();
-				}
-				else
-				{
-					//testing text/background color
-					system("clear");
-					system("color 9F");
-
-					//Joshs code can go here
-					cout << "Josh Code";
-				}
+			system("CLS");
+			if (adminStatus == true)
+			{
+				test.AdminSelection();
+			}
+			else
+			{
+				MainMenu();
+			}
 		}
 		test.CloseAccounts();
+
+
+
 	}
 
 	return 0;
 }
-//here's a new comment.
+
