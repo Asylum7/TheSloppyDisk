a727d4838b5eae44d653d7dffeb44784adc1629c
Talon Baker <taalonn@gmail.com>
2014-12-09 13:40:58 -0800

    Help function edit
    added spacing to the help function output

--- help.cpp
+++ help.cpp
@@ -7,13 +7,13 @@ void Help()
 	//printing help header and help color
 	HelpMenuHeader();
 
-	cout << "You are being redirected to an online help tutorial!\n";
+	cout << "You are being redirected to an online help tutorial!\n\n";
 
 	//Opening external google docs link to an interactive help menu
 	ExternalLink("https://docs.google.com/document/d/1YoCcngpTv694s-"
 			     "Jo6ML1giQqiH-_BI9D4yEXM0nytvI/edit?usp=sharing");
 
-	cout << "For other help options please contact us at:\n";
+	cout << "For other help options please contact us at:\n\n";
 	cout << "Phone:   1-800-ROBOTCARE\n";
 	cout << "Email:   iRobotsTheBomb.com@gmail.com\n";
 	cout << "Fax:     1-888-ROBOTFAXS\n\n";
